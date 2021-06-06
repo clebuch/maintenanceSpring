920603dddbec796eda51c4dcf4207705fbf46fc4
Clemens <clemens.buchner@gmail.com>
2021-06-06 23:37:18 +0200

    log

--- src/main/java/jku/se/maintenance/MaintenanceApplication.java
+++ src/main/java/jku/se/maintenance/MaintenanceApplication.java
@@ -1,14 +1,19 @@
 package jku.se.maintenance;
 
-import org.springframework.boot.CommandLineRunner;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class MaintenanceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MaintenanceApplication.class, args);
+
+		log.info("Swagger API UI:");
+		log.info("Remote: https://maintenance-spring.herokuapp.com/swagger-ui.html");
+		log.info("Local:  http://localhost:8080/swagger-ui.html");
 	}
 
 }
